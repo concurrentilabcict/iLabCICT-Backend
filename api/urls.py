34acf40d75fca077e9c3370618d4b2b14d420296
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-06 23:05:03 +0800

    update: implemented custom login response

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from rest_framework_simplejwt.views import TokenRefreshView
+from api.user.jwt import CustomTokenObtainPairSerializer
 
 urlpatterns = [
-    path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/login/", CustomTokenObtainPairSerializer.as_view(), name="token_obtain_pair"),
     path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     path('user/', include('api.user.urls'))
--- api/user/views.py
+++ api/user/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from api.user.models import User
 from api.user.serializers import UserSerializer
+from api.user.jwt import CustomTokenObtainPairSerializer
+
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 class UserListCreateView(ListCreateAPIView):
     queryset = User.objects.all()
@@ -9,3 +12,6 @@ class UserListCreateView(ListCreateAPIView):
 class UserDetailView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
