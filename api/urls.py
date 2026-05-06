86393d58bca266ce970c71bf7c6db20ea63ded28
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-06 23:13:26 +0800

    update: revert to default tokenobtainpairview

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
-from api.user.jwt import CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.views import (TokenObtainPairView ,TokenRefreshView)
 
 urlpatterns = [
-    path("auth/login/", CustomTokenObtainPairSerializer.as_view(), name="token_obtain_pair"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     path('user/', include('api.user.urls'))
--- api/user/views.py
+++ api/user/views.py
@@ -1,7 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from api.user.models import User
 from api.user.serializers import UserSerializer
-from api.user.jwt import CustomTokenObtainPairSerializer
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -12,6 +11,3 @@ class UserListCreateView(ListCreateAPIView):
 class UserDetailView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
