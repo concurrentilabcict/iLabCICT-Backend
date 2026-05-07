77836423c480cac13a74083fc9cc4239a9155adf
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-07 12:39:55 +0800

    update: ticket model

--- api/ticket/model.py
+++ api/ticket/model.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 class Ticket(models.Model):
+    class TicketStatus(models.TextChoices):
+        OPEN = "open", "open"
+        PENDING = "pending", "pending"
+        RESOLVED = "resolved", "resolved"
+        
+    title = models.CharField(max_length=100)
+    complaint_description = models.TextField()
+
+    status = models.CharField(max_length=20, choices=TicketStatus.choices, default=TicketStatus.OPEN)
+
+    created_at = models.DateTimeField(auto_now_add=True)
