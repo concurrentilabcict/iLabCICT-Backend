73e6273920f00ae0a095981aab05e428e513de04
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-11 02:14:12 +0800

    update: added dynamic value on room serializer

--- api/room/models.py
+++ api/room/models.py
@@ -9,7 +9,6 @@ class Room(models.Model):
 
     room_name = models.CharField(max_length=20)
     floor_number = models.IntegerField()
-    computer_count = models.IntegerField(default=0)
     status = models.CharField(max_length=20, choices=RoomStatus.choices, default=RoomStatus.OPERATIONAL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
