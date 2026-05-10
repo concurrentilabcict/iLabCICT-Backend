46fc23e58caa12ad21fa30d416b2e6aa56d9b96c
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-11 01:58:36 +0800

    update: finished ticket model

--- api/ticket/models.py
+++ api/ticket/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+from api.computer.models import Computer
+from api.room.models import Room
 
 class Ticket(models.Model):
     class TicketStatus(models.TextChoices):
@@ -6,11 +9,20 @@ class Ticket(models.Model):
         PENDING = "pending", "pending"
         RESOLVED = "resolved", "resolved"
         
+    class TicketType(models.TextChoices):
+        REPORT = "report", "report"
+        REQUEST = "request", "request"    
+
+    reported_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reported_tickets")
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="assigned_tickets")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="tickets")
+    computer = models.ForeignKey(Computer, on_delete=models.CASCADE, related_name="tickets")
+
+    type = models.CharField(max_length=12, choices=TicketType.choices)
     title = models.CharField(max_length=100)
     complaint_description = models.TextField()
-
     issue_image = models.ImageField(upload_to="tickets/", null=True, blank=True)
-
     status = models.CharField(max_length=20, choices=TicketStatus.choices, default=TicketStatus.OPEN)
 
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
