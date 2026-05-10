901e2a0cfbe8da61de30bef5b7596cf0665b90ef
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-11 02:13:49 +0800

    update: added dynamic value on room serializer

--- api/room/serializers.py
+++ api/room/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from api.room.models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
+
+    computer_count = serializers.SerializerMethodField()
+    
     class Meta:
         model = Room
         fields = '__all__'
+
+    def get_computer_count(self, obj):
+        return obj.computers.count()
