b293e2d5b243edb064e639d12f50070415b60ee2
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-11 02:08:14 +0800

    update: added room status choices

--- api/room/models.py
+++ api/room/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 class Room(models.Model):
+    class RoomStatus(models.TextChoices):
+        OPERATIONAL = "operational", "operational"
+        MAINTENANCE = "maintenance", "maintenance"
+        DEGRADED = "degraded", "degraded"
+        OUT_OF_SERVICE = "out_of_service", "out of service"
+
     room_name = models.CharField(max_length=20)
     floor_number = models.IntegerField()
-    computer_count = models.IntegerField()
-    status = models.BooleanField()
+    computer_count = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, choices=RoomStatus.choices, default=RoomStatus.OPERATIONAL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
