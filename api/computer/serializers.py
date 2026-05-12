b514596d59b7b9c32138809ea9b6e17b36896ce3
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-12 21:24:25 +0800

    update: made read only for unique entity codes on ticket and computer

--- api/computer/serializers.py
+++ api/computer/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from api.computer.models import Computer
 
 class ComputerSerializer(serializers.ModelSerializer):
+    computer_code = serializers.CharField(read_only=True)
     class Meta:
         model = Computer
         fields = '__all__'
--- api/ticket/serializers.py
+++ api/ticket/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from api.ticket.models import Ticket
 
 class TicketSerializer(serializers.ModelSerializer):
+    ticket_code = serializers.CharField(read_only=True)
     class Meta:
         model = Ticket
         fields = '__all__'
