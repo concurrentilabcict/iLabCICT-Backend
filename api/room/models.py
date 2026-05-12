d2e8e8918a2fa171ba05c6faa30f3f95cede8f5e
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-05-12 21:14:24 +0800

    update: added building name on room

--- api/room/models.py
+++ api/room/models.py
@@ -7,6 +7,12 @@ class Room(models.Model):
         DEGRADED = "degraded", "degraded"
         OUT_OF_SERVICE = "out_of_service", "out of service"
 
+    class BuildingName(models.TextChoices):
+        PIMENTEL = "pimentel", "pimentel"
+        LAW = "law", "law"
+        ACAD = "acad", "acad"
+
+    building_name = models.CharField(max_length=20, choices=BuildingName.choices, default=BuildingName.PIMENTEL)
     room_name = models.CharField(max_length=20, unique=True)
     floor_number = models.IntegerField()
     status = models.CharField(max_length=20, choices=RoomStatus.choices, default=RoomStatus.OPERATIONAL)
