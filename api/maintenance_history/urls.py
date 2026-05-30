124421ace3ce71e9085b7659b83a042fc0c511f3
Polstuds27 <paul.mojal@gmail.com>
2026-05-30 22:33:02 +0800

    fix: removed delete and update operations in maintenance history

--- api/maintenance_history/urls.py
+++ api/maintenance_history/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from api.maintenance_history.views import MaintenanceHistoryListCreateView, MaintenanceHistoryByComputerCode, MaintenanceHistoryDetailView
+from api.maintenance_history.views import MaintenanceHistoryListCreateView, MaintenanceHistoryByComputerCode
 
 urlpatterns = [
     path('', MaintenanceHistoryListCreateView.as_view()),
-    path('<int:pk>/', MaintenanceHistoryDetailView.as_view()),
+    path('<int:pk>/', MaintenanceHistoryListCreateView.as_view()),
 
     path('computer/', MaintenanceHistoryByComputerCode.as_view()),
 ]
