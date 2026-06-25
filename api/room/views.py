1423e5bc69c77e16cf3390f8e8b3461e1a19b759
Polstuds27 <paul.mojal@gmail.com>
2026-06-25 17:43:52 +0800

    update: added filter params on room api

--- api/room/views.py
+++ api/room/views.py
@@ -4,30 +4,17 @@ from api.room.serializers import RoomSerializer
 from api.room.services import RoomService
 
 class RoomListCreateView(ListCreateAPIView):
-    queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+    def get_queryset(self):
+        return RoomService.get_all(
+            status=self.request.query_params.get('status'),
+            building=self.request.query_params.get('building_name'),
+            room=self.request.query_params.get('room_name')
+        )
+    
+    
 class RoomDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
-class RoomStatusListView(ListAPIView):
-    serializer_class = RoomSerializer
-
-    def get_queryset(self):
-        status = self.request.query_params.get("status")
-        return RoomService.get_all_by_status(status)
-
-class RoomPerBuildingListView(ListAPIView):
-    serializer_class = RoomSerializer
-
-    def get_queryset(self):
-        building = self.request.query_params.get('building_name')
-        return RoomService.get_all_by_building(building)
-
-class RoomPerNameListView(ListAPIView):
-    serializer_class = RoomSerializer
-
-    def get_queryset(self):
-        room = self.request.query_params.get('room_name')
-        return RoomService.get_all_by_room_name(room)
