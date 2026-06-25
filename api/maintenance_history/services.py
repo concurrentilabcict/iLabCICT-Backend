a8bba210d9392d01c6d9dfb4ae84323a4a8cca72
Polstuds27 <paul.mojal@gmail.com>
2026-06-25 18:20:15 +0800

    update: added filter params on maintenance history api

--- api/maintenance_history/services.py
+++ api/maintenance_history/services.py
@@ -1,8 +1,25 @@
 
 from api.maintenance_history.models import MaintenanceHistory
 class MaintenanceHistoryServices:
-
+    
     @staticmethod
-    def get_all_maintenance_history_by_computer(computer_code=None):
-        queryset = MaintenanceHistory.objects.filter(computer=computer_code)
+    def get_all(type=None,
+                computer_id=None,
+                technician_id=None,
+                date=None):
+        
+        queryset = MaintenanceHistory.objects.all()
+
+        if type is not None:
+            queryset = queryset.filter(maintenance_type=type)
+        
+        if computer_id is not None:
+            queryset = queryset.filter(computer_id=computer_id)
+
+        if technician_id is not None:
+            queryset = queryset.filter(technician_id=technician_id)
+
+        if date is not None:
+            queryset = queryset.filter(date_performed__date=date)
+
         return queryset
--- api/maintenance_history/urls.py
+++ api/maintenance_history/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from api.maintenance_history.views import MaintenanceHistoryListView, MaintenanceHistoryByComputerCode, MaintenanceHistoryDetailView
+from api.maintenance_history.views import MaintenanceHistoryListView , MaintenanceHistoryDetailView
 
 urlpatterns = [
     path('', MaintenanceHistoryListView.as_view()),
     path('<int:pk>/', MaintenanceHistoryDetailView.as_view()),
 
-    path('computer/', MaintenanceHistoryByComputerCode.as_view()),
 ]
