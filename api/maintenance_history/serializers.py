5dc9b84278945ce702c930204cb3a380c4895407
Polstuds27 <paul.mojal@gmail.com>
2026-07-17 17:53:28 +0800

    update: added computer code param on maintenance history

--- api/maintenance_history/serializers.py
+++ api/maintenance_history/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from api.maintenance_history.models import MaintenanceHistory
 from api.repair_log.serializers import RepairLogReadSerializer
+from api.computer.serializers import ComputerMinimalSerializer
 
 class MaintenanceHistorySerializer(serializers.ModelSerializer):
 
     repair_log = RepairLogReadSerializer(read_only=True)
+    computer = ComputerMinimalSerializer()
 
     class Meta:
         model = MaintenanceHistory
