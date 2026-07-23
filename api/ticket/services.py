31edf3824f629372103b2c7f884e16427e773f43
Polstuds27 <paul.mojal@gmail.com>
2026-07-23 11:57:00 +0800

    update: updated faculty stats

--- api/ticket/services.py
+++ api/ticket/services.py
@@ -2,7 +2,6 @@ from api.ticket.models import Ticket
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync  
 from api.notification.services import NotificationService
-from api.ticket.serializers import TicketReadSerializer
 from django.db import transaction
 from api.common.utils.date_checker import is_invalid_date_format
 from rest_framework.exceptions import ValidationError
@@ -92,16 +91,6 @@ class TicketService:
             ticket_id=ticket.id
         )
 
-        channel_layer = get_channel_layer()
-
-        async_to_sync(channel_layer.group_send)(
-            'technicians',
-            {
-                'type': 'ticket_created',
-                'ticket': TicketReadSerializer(ticket).data
-            }
-        )
-
         return ticket
     
     @staticmethod
@@ -115,20 +104,11 @@ class TicketService:
             'computer'
         ).get(pk=ticket.pk)
 
-        channel_layer = get_channel_layer()
 
         NotificationService.create_new_ticket_notification(
             receiver_id=ticket.reported_by,
             title='Ticket Status Updated!',
             ticket_id=ticket.id
-        )
-
-        async_to_sync(channel_layer.group_send)(
-            'technicians',
-            {
-                'type': 'ticket_updated',
-                'ticket': TicketReadSerializer(ticket).data
-            }
         )
 
         return ticket
@@ -149,32 +129,44 @@ class TicketService:
             }
         )
 
+    
     @staticmethod
-    @transaction.atomic
-    def claim_ticket(ticket_id, technician, status):
+    @transaction.atomic()
+    def claim_ticket(instance, technician, status):
         updated = (
-            Ticket.objects
-            .filter(
-                id=ticket_id,
-                assigned_to__isnull=True
-            )
-            .update(
-                assigned_to=technician,
-                status=status
-            )
-        )
+                    Ticket.objects
+                    .filter(
+                        id=instance.id,
+                        assigned_to__isnull=True
+                    )
+                    .update(
+                        assigned_to=technician,
+                        status=status
+                    )
+                )
 
         if updated:
-            return Ticket.objects.get(id=ticket_id)
+            return Ticket.objects.select_related("reported_by",
+                                "assigned_to",
+                                "room",
+                                "computer"
+                            ).get(id=instance.id)
 
-        ticket = Ticket.objects.get(id=ticket_id)
-
-        if ticket.assigned_to != technician:
+        instance.refresh_from_db()
+        
+        if instance.assigned_to != technician:
             raise ValidationError("Ticket has already been claimed.")
 
-        ticket.status = status
-        ticket.save(update_fields=["status"])
+        instance.status = status
+        instance.save(update_fields=["status"])
 
-        return Ticket.objects.select_related(
-            "reported_by", "assigned_to", "room", "computer"
-        ).get(id=ticket_id)
+        return instance
+
+    @staticmethod
+    def update_ticket_to_ongoing(instance):
+        instance.status = Ticket.TicketStatus.ONGOING
+        instance.save(update_fields=["status"])
+        return instance
+
+
+    
