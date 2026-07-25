2a17f572fe781c20f6f9774213ab0bbdfa9dcdae
Polstuds27 <paul.mojal@gmail.com>
2026-07-25 23:41:16 +0800

    update: updated yaml file

--- api/task_scheduler/view.py
+++ api/task_scheduler/view.py
@@ -1,11 +1,22 @@
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.views import APIView
 from api.task_scheduler.models import TaskScheduler
 from api.task_scheduler.serializer import TaskSchedulerSerializer
 from api.permissions import IsAdmin
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import IsAuthenticated
+
+class CreateTaskSchedulerView(CreateAPIView):
+    serializer_class=TaskSchedulerSerializer
+    queryset=TaskScheduler.objects.all()
+    permission_classes= [IsAuthenticated, IsAdmin]
+
 class TaskSchedulerDetailView(RetrieveUpdateAPIView):
     serializer_class=TaskSchedulerSerializer
     queryset = TaskScheduler.objects.all()
     permission_classes = [IsAuthenticated, IsAdmin]
+
+class SchedulerView(APIView):
+    permission_classes=[]
+    ...
         
 
