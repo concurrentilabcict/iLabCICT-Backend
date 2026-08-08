112a8f8b244b003f198004de321170c72eb6c33b
Polstuds27 <paul.mojal@gmail.com>
2026-08-08 20:20:33 +0800

    update: added websocket on report

--- api/routing.py
+++ api/routing.py
@@ -2,9 +2,11 @@ from django.urls import path
 from api.ticket.consumers import TicketConsumer
 from api.notification.consumers import NotifcationConsumer
 from api.room.consumers import RoomConsumer
+from api.report.consumers import ReportConsumer
 
 websocket_urlpatterns = [
     path('ws/tickets/', TicketConsumer.as_asgi()),
     path('ws/notifications/', NotifcationConsumer.as_asgi()),
-    path('ws/rooms/', RoomConsumer.as_asgi())
+    path('ws/rooms/', RoomConsumer.as_asgi()),
+    path('ws/reports', ReportConsumer.as_asgi())
 ]
